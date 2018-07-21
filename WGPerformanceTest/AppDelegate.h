3d73cceb6c297af0c8e45b0916f00c451f4f5613
wanggang <978159546@qq.com>
2018-07-21 20:30:50 +0800

    Initial Commit

---
+++ WGPerformanceTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGPerformanceTest
+//
+//  Created by wanggang on 2018/7/21.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGPerformanceTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGPerformanceTest
+//
+//  Created by wanggang on 2018/7/21.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
